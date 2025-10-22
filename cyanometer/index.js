f2d44cba223660d66191bc62858550e2790b737f
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2025-10-22 15:59:40 -0400

    mobile improvements

--- cyanometer/index.js
+++ cyanometer/index.js
@@ -11,3 +11,16 @@ infoToggle.addEventListener("click", () => {
         infoToggle.innerHTML = "+ what is a cyanometer?";
     }
 });
+
+
+// resize vh for mobile
+mobileResize();
+
+function mobileResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', () => {
+    mobileResize();
+});
